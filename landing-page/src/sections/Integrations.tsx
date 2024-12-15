758e8ea8a60157d7c6816da793812cffc53f5015
chiragsingh1 <imchirag.2717@gmail.com>
2024-12-15 23:16:03 +0530

    Integrations component infinite scroll animation"

--- landing-page/src/sections/Integrations.tsx
+++ landing-page/src/sections/Integrations.tsx
@@ -31,6 +31,7 @@ export default function Integrations() {
                             <IntegrationsColumn
                                 integrations={integrations.slice().reverse()}
                                 className="hidden md:flex"
+                                reverse
                             />
                         </div>
                     </div>
